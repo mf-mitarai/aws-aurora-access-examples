7806080865b25ba92f4e2e19deaf10170997092c
Hidenobu Mitarai <hidenobu_mitarai@mforce.co.jp>
2023-05-26 10:40:57 +0900

    PostgreSQLに変更

--- src/main/java/jp/co/mforce/si/app01/Employee.java
+++ src/main/java/jp/co/mforce/si/app01/Employee.java
@@ -2,10 +2,12 @@ package jp.co.mforce.si.app01;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "employee", schema = "examples")
 public class Employee {
 
 	@Id
